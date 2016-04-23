25e2d52e6695a31b582a584f1710cbe19f459c4f
James Skimming <JSkimming@users.noreply.github.com>
2016-04-23 21:20:05 +0100

    Added empty console app

---
+++ src/HttpClient.Dispose/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpClientDispose
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
